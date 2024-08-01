77cfa02e70935258bd7f2e2ff18e436225bbea49
moniquevdor <149339455+moniquevdor@users.noreply.github.com>
2024-08-01 13:45:59 +1000

    5_rundiamond.sh

--- AnnotationPipeline/4_FunctionnalAnnotation/5_rundiamond.sh
+++ AnnotationPipeline/4_FunctionnalAnnotation/5_rundiamond.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-#SBATCH -J Diamond             
-#SBATCH -o Diamond.out      
-#SBATCH -e Diamond.e  
-#SBATCH --account=tn20 
-#SBATCH -n 1                
-#SBATCH -c 20                
-#SBATCH --mem-per-cpu=10G
-#SBATCH --mail-user=monique.vandorssen@monash.edu 
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-#SBATCH -t 24:00:00
 
-
-./diamond blastp --verbose --tmpdir /fs03/tn20/Monique/referencegenome/6_Functional_Annotation/Hap1/5_diamond/blast_func/diamondtmp/ --db /fs03/tn20/Monique/referencegenome/6_Functional_Annotation/Hap1/5_diamond/blast_func/nr/nr --out /fs03/tn20/Monique/referencegenome/6_Functional_Annotation/Hap1/5_diamond/blast_func/diamond_out.txt --header simple --max-target-seqs 5 --query /fs03/tn20/Monique/referencegenome/6_Functional_Annotation/Hap1/4_cleanup/Galeolaria_caespitosa_proteins_final.fasta --max-hsps 1 --outfmt 6 qseqid qlen sseqid slen stitle pident evalue 
+./diamond blastp --verbose --tmpdir {your_path}/blast_func/diamondtmp --db {your_path}/blast_func/nr/nr --out {your_path}/blast_func/diamond_out.txt --header simple --max-target-seqs 5 --query {your_path}/Galeolaria_caespitosa_proteins_final.fasta --max-hsps 1 --outfmt 6 qseqid qlen sseqid slen stitle pident evalue 
 
 #Filter diamond hits on R with 1_filter_diamond_hits.R
