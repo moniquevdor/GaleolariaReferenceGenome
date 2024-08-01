4fb9f6ae439277329dcdeb3624cb2e22dad61d85
moniquevdor <149339455+moniquevdor@users.noreply.github.com>
2024-08-01 13:44:33 +1000

    3_runFunannotate.sh

--- AnnotationPipeline/4_FunctionnalAnnotation/3_runFunannotate.sh
+++ AnnotationPipeline/4_FunctionnalAnnotation/3_runFunannotate.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
-#SBATCH -J FunctionalAnnot             
-#SBATCH -o FunctionalAnnot.out      
-#SBATCH -e FunctionalAnnot.e  
-#SBATCH --account=tn20 
-#SBATCH -n 1                
-#SBATCH -c 20                
-#SBATCH --mem-per-cpu=10G
-#SBATCH --mail-user=monique.vandorssen@monash.edu 
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-#SBATCH -t 24:00:00
-
 
 # Use Funannotate to combine InterProScan and Eggnog annotations and generate additional functional annotations
 funannotate annotate \
   --sbt template.sbt \
-  --gff /fs03/tn20/Monique/referencegenome/6_Functional_Annotation/Hap1/FinalGeneSet_fixed_sorted.gff \
-  --mito-pass-thru /fs03/tn20/Monique/referencegenome/9_MitoAssembly/Results_combined/final_mitogenome.fasta \
-  --fasta /fs03/tn20/Monique/referencegenome/6_Functional_Annotation/Hap1/ChromAssemblyHap1Final_masked.fasta \
+  --gff {your_path}/FinalGeneSet_fixed_sorted.gff \
+  --mito-pass-thru {your_path}/final_mitogenome.fasta \
+  --fasta {your_path}/ChromAssemblyHap1Final_masked.fasta \
   --species "Galeolaria caespitosa" \
-  --out /fs03/tn20/Monique/referencegenome/6_Functional_Annotation/Hap1/3_funannotate/annotations \
-  --iprscan /fs03/tn20/Monique/referencegenome/6_Functional_Annotation/Hap1/1_interproscan/Galeolaria_caespitosa_proteins.fasta.xml \
-  --eggnog /fs03/tn20/Monique/referencegenome/6_Functional_Annotation/Hap1/2_eggnog/GaleoEggNog.emapper.annotations \
+  --out {your_path}/annotations \
+  --iprscan {your_path}/Galeolaria_caespitosa_proteins.fasta.xml \
+  --eggnog {your_path}/GaleoEggNog.emapper.annotations \
   --force \
   --database funannotate_db \
-  --busco_db /fs03/tn20/Monique/referencegenome/6_Functional_Annotation/Hap1/3_funannotate/funannotate_db/metazoa
+  --busco_db {your_path}/funannotate_db/metazoa
   
