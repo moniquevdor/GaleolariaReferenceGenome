70d2ab07fbf4300607f694e534a676ada7f27889
moniquevdor <149339455+moniquevdor@users.noreply.github.com>
2024-08-01 13:43:07 +1000

    1_runInterproscan.sh

--- AnnotationPipeline/4_FunctionnalAnnotation/1_runInterproscan.sh
+++ AnnotationPipeline/4_FunctionnalAnnotation/1_runInterproscan.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-#SBATCH -J Interpro             
-#SBATCH -o Interpro.out      
-#SBATCH -e Interpro.e  
-#SBATCH --account=tn20 
-#SBATCH -n 1                
-#SBATCH -c 20                
-#SBATCH --mem-per-cpu=10G
-#SBATCH --mail-user=monique.vandorssen@monash.edu 
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-#SBATCH -t 24:00:00
 
 module load interproscan
 
 # Generate functional annotations using InterProScan
-interproscan.sh -i ../Galeolaria_caespitosa_proteins.fasta \
+interproscan.sh -i {your_path}/Galeolaria_caespitosa_proteins.fasta \
             -f xml,gff3 \
             -goterms \
             --pathways \
