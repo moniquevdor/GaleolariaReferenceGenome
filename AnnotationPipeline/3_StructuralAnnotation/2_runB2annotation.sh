c415c699abc3e8a010da422b0e21ff483730d9b1
moniquevdor <149339455+moniquevdor@users.noreply.github.com>
2024-08-01 13:38:50 +1000

    2_runB2annotation.sh

--- AnnotationPipeline/3_StructuralAnnotation/2_runB2annotation.sh
+++ AnnotationPipeline/3_StructuralAnnotation/2_runB2annotation.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
-#SBATCH -J Braker2             
-#SBATCH -o Braker2.out      
-#SBATCH -e Braker2.e  
-#SBATCH --account=tn20    
-#SBATCH -n 1                  
-#SBATCH -c 35                                
-#SBATCH --mail-user=monique.vandorssen@monash.edu 
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-#SBATCH -t 7-00:00:00
 
 module load singularity
 
 SING="singularity run -B /fs02 -B /fs03 -B /fs04 -B /scratch -B /projects ../../braker3.sif"
 
-wdir=/fs03/tn20/Monique/referencegenome/5_Annotation/Annotation_H1/
+wdir={your_path_to_directory}
 
-$SING braker.pl --AUGUSTUS_CONFIG_PATH=/fs03/tn20/Monique/referencegenome/5_Annotation/Augustus/config --COMPLEASM_PATH=/fs03/tn20/Monique/referencegenome/5_Annotation/compleasm_kit/ --GENEMARK_PATH=/fs03/tn20/Monique/referencegenome/5_Annotation/GeneMark-ETP/bin/ --species=GaleoHap1 --useexisting --genome=../ChromAssemblyHap1Final_masked.fasta --softmasking --prot_seq=../AnnelidProtein.fasta --workingdir=$wdir/braker2
-
-# --epmode 
+$SING braker.pl --AUGUSTUS_CONFIG_PATH={your_path}/Augustus/config --COMPLEASM_PATH={your_path}/compleasm_kit/ --GENEMARK_PATH={your_path}/GeneMark-ETP/bin/ --species=GaleoHap1 --useexisting --genome={your_path}/ChromAssemblyHap1Final_masked.fasta --softmasking --prot_seq={your_path}/AnnelidProtein.fasta --workingdir=$wdir/braker2
